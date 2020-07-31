16491c45a14f284c347e1a3252589ec4521d1c49
lufovic77 <lufovic77@gmail.com>
2020-07-31 10:45:27 +0000

    class on python

---
+++ class.py
@@ -0,0 +1,13 @@
+class Man:
+    def __init__(self, name): #Same as constructor in CPP
+        self.name = name
+        print("init done")
+
+    def hello(self): #Presenting 'self' as the first parameter of method is common.
+        print("Hello "+self.name+ "!")
+
+    def bye(self):
+        print("Bye "+ self.name +"!")
+m = Man("David") #Calling __init__ method
+m.hello()
+m.bye()
