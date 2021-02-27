b5e0218aaab873e6ee66e2a0cc499be211c0e0c6
Kyungmin Kim <lufovic77@gmail.com>
2021-02-27 06:21:38 +0000

    모듈화를 위해서 함수로 빼냄

--- CH3_neuralNetwork/activeFunc.py
+++ CH3_neuralNetwork/activeFunc.py
@@ -1,32 +1,27 @@
 import numpy as np
-class activation:
-    def __init__(self):
-        print("initialized")
-    def stepVer1(self, x):#Only with scalar 
-        if(x>0):
-            return 1
-        else:
-            return 0
-    def stepVer2(self, x):
-        y = x>0#bool numpy array
-        return y.astype(np.int)#Convert to int array
-    def sigmoid(self, x):
-        return 1 / (1+np.exp(-x))
-    def ReLu(self, x):
-        return np.maximum(0, x)
-class Softmax:
-    def __init__(self):
-        print("init")
-    def noSoftmax(self, arr):
-        exp = np.exp(arr)
-        exp_sum = np.sum(exp)
-        return exp/exp_sum
-    def softmax(self, arr):
-        arr_max = np.max(arr) #overflow
-        arr -= arr_max
-        exp = np.exp(arr)
-        exp_sum = np.sum(exp)
-        return exp/exp_sum
+def stepVer1(x):#Only with scalar 
+    if(x>0):
+        return 1
+    else:
+        return 0
+def stepVer2(x):
+    y = x>0#bool numpy array
+    return y.astype(np.int)#Convert to int array
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+def ReLu(self, x):
+    return np.maximum(0, x)
+def noSoftmax(arr):
+    exp = np.exp(arr)
+    exp_sum = np.sum(exp)
+    return exp/exp_sum
+def softmax(arr):
+    arr_max = np.max(arr) #overflow
+    arr -= arr_max
+    exp = np.exp(arr)
+    exp_sum = np.sum(exp)
+    return exp/exp_sum
+"""
 soft = Softmax()
 tmp = np.arange(1, 2, 0.1)
 print(tmp)
@@ -39,3 +34,4 @@ print("step func: ")
 print(A.stepVer2(x))
 print("sigmoid: " )
 print(A.sigmoid(x))
+"""
